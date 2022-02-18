b9a8ffa9d962c7bffaad5ad5107a27920758e12f
noname2048 <sungwook.csw@gmail.com>
2022-02-18 23:05:30 +0900

    style: change styles

--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,47 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>MyBooks</title>
-        <meta name="description" content="you can display your books in home" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header>
-        <logo className={styles.logo}>MyBooks</logo>
-        <ul className={styles.centerButton}>
-          <li>Search</li>
-          <li>Request</li>
-        </ul>
-      </header>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className={styles.compact}>
+        <Head>
+          <title>MyBooks</title>
+          <meta
+            name="description"
+            content="you can display your books in home"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <header className={styles.header}>
+          <logo className={styles.logo}>MyBooks</logo>
+          <ul className={styles.centerButton}>
+            <li className={styles.left}>Search</li>
+            <li className={styles.right}>Request</li>
+          </ul>
+        </header>
+        <div className={styles.main}>
+          <div>
+            <h2>
+              <span>제목</span>으로 검색하기
+            </h2>
+            <input type="text" name="" id="" />
+          </div>
+          <div>
+            <h2>
+              <span>ISBN</span>으로 검색하기
+            </h2>
+            <input type="text" name="" id="" />
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <div className={styles["detail-search"]}>상세 검색하기</div>
+        <footer className={styles.footer}>
+          <p>@ 2022</p>{" "}
+          <p>해당 웹사이트는 알라딘 API를 이용하여 만들어졌습니다.</p>
+          <p>sungwook.csw@gmail.com</p>
+        </footer>
+      </div>
     </div>
   );
 }
