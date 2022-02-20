98ee30856490dee16e232a4218d7ec3afe3d68d9
noname2048 <sungwook.csw@gmail.com>
2022-02-20 20:11:44 +0900

    feat: micro improve for design in Indexheader

--- components/IndexHeader.js
+++ components/IndexHeader.js
@@ -4,18 +4,22 @@ import { BsFillFilePlusFill } from "react-icons/bs";
 export default function IndexHeader() {
   return (
     <div className={styles.header}>
-      <div className={styles["header-left"]}></div>
-      <div className={styles["header-center"]}>
-        <div className={styles["button-container"]}>
-          <div className={styles["button-left-container"]}>
-            <BiSearch size={"3rem"}></BiSearch>
-          </div>
-          <div className={styles["button-right-container"]}>
-            <BsFillFilePlusFill size={"3rem"}></BsFillFilePlusFill>
-          </div>
-        </div>
-      </div>
-      <div className={styles["header-right"]}></div>
+      <logo className={styles.logo}>MyBooks</logo>
+      <label htmlFor="" className={styles.label}>
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          className={styles.input}
+          onChange={() => console.log("hi")}
+          checked
+        />
+        <span className={styles.span}>
+          <BiSearch size={"3rem"} className={styles.icon}></BiSearch>
+        </span>
+        {/* <span className={styles.span}></span> */}
+      </label>
+      <label htmlFor="">github</label>
     </div>
   );
 }
