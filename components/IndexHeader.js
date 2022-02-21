973f1a3305464a7ebdd4fc54d7807a2fd2a47445
noname2048 <sungwook.csw@gmail.com>
2022-02-21 19:27:00 +0900

    feat: install classNames and change styling

--- components/IndexHeader.js
+++ components/IndexHeader.js
@@ -2,22 +2,38 @@ import styles from "./IndexHeader.module.scss";
 import { BiSearch } from "react-icons/bi";
 import { BsFillFilePlusFill } from "react-icons/bs";
 import { GiBlackBook } from "react-icons/gi";
+import { SiGithub } from "react-icons/si";
+import { useState } from "react";
+import classNames from "classnames/bind";
+
+var cx = classNames.bind(styles);
 
 export default function IndexHeader() {
+  const [tabname, setTabname] = useState("search");
+
   return (
     <div className={styles.header}>
       <logo className={styles.logo}>
         <GiBlackBook />
         MyBooks
       </logo>
-      <label className={styles.label}>
-        <input type="checkbox" name="" />
-        <span>
+      <div className={styles["toogle-tab"]}>
+        <div
+          onClick={() => setTabname("search")}
+          className={cx({ on: tabname === "search" })}
+        >
           <BiSearch />
-        </span>
-        <span></span>
-      </label>
-      <label htmlFor="">github</label>
+          search
+        </div>
+        <div
+          onClick={() => setTabname("request")}
+          className={cx({ on: tabname === "request" })}
+        >
+          <BsFillFilePlusFill />
+          request
+        </div>
+      </div>
+      <SiGithub />
     </div>
   );
 }
