b44e850775e7a509be298c320cda0ce04f41b7bc
noname2048 <sungwook.csw@gmail.com>
2022-02-21 19:27:34 +0900

    feat: add techstack in footer

--- pages/index.js
+++ pages/index.js
@@ -50,6 +50,7 @@ export default function Home() {
           <p>@ 2022</p>{" "}
           <p>해당 웹사이트는 알라딘 API를 이용하여 만들어졌습니다.</p>
           <p>sungwook.csw@gmail.com</p>
+          <p>nextjs + fastapi</p>
         </footer>
       </div>
     </div>
