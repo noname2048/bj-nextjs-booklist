5d291023ec97dfb0c23b1ae56bce877602c67615
noname2048 <sungwook.csw@gmail.com>
2022-02-23 20:37:49 +0900

    feat: delete page, change styling

--- components/newBookRequest.js
+++ components/newBookRequest.js
@@ -1,3 +1,17 @@
+import styles from "../styles/Home.module.scss";
+
 export default function NewBookRequest() {
-  return <div>NewRequest</div>;
+  return (
+    <div className={styles.newrequest}>
+      <h1>등록요청하기</h1>
+      <div className={styles.form}>
+        <span>isbn</span>
+        <label>
+          <input type="text" />
+          <span>검색</span>
+        </label>
+      </div>
+      <button>요청</button>
+    </div>
+  );
 }
--- pages/newrequest.js
+++
@@ -1,5 +0,0 @@
-import styles from "../styles/Home.module.scss";
-
-export default function Page() {
-  return <div className={styles.container}></div>;
-}
