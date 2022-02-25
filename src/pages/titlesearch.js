fb729bf6f4962076e6fcafab6d4ebcaa211c4719
noname2048 <sungwook.csw@gmail.com>
2022-02-25 23:18:38 +0900

    styles: styles list

--- src/pages/titlesearch.js
+++ src/pages/titlesearch.js
@@ -1,22 +1,36 @@
 import Link from "next/link";
 import Router from "next/router";
-import styles from "../../styles/Home.module.scss";
+import styles from "../../styles/List.module.scss";
+import { BsSearch } from "react-icons/bs";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Page() {
+  const router = useRouter();
+  useEffect(() => {
+    (async function getSearch() {
+      await fetch("localhost:8000/search/title/");
+    })();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Link href="/">
-        <a>home</a>
-      </Link>
       <div className={styles.navigation}>
-        <span>title</span>
-        <input
-          type="text"
-          onKeyPress={(event) => {
-            if (event.key == "Enter") Router.push("/titlesearch");
-          }}
-        />
-        <span>button</span>
+        <Link href="/">
+          <a>home</a>
+        </Link>
+        <div className={styles.searchbox}>
+          <span>titlesearch</span>
+          <label>
+            <input
+              type="text"
+              onKeyPress={(event) => {
+                if (event.key == "Enter") Router.push("/titlesearch");
+              }}
+            />
+            <BsSearch />
+          </label>
+        </div>
       </div>
       <div className={styles["list-container"]}>
         <div className={styles.card}></div>
