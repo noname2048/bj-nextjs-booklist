a2860f557f4ad03cdbdd554548b890985499b9c2
noname2048 <sungwook.csw@gmail.com>
2022-03-15 16:52:14 +0900

    feat: make fetch and log

--- src/components/NewBookRequest.js
+++ src/components/NewBookRequest.js
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import styles from "./IndexMain.module.scss";
 
 export default function NewBookRequest() {
+  const [userIsbnValue, setUserIsbnValue] = useState("");
   return (
     <div className={styles.newrequest}>
       <h1>등록요청하기</h1>
       <div className={styles.form}>
         <span>isbn</span>
         <label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(e) => {
+              setUserIsbnValue(e.target.value);
+            }}
+          />
           <span>검색</span>
         </label>
       </div>
-      <button>요청</button>
+      <button
+        onClick={() => {
+          fetch("http://localhost:8000/requests", {
+            method: "POST",
+            body: { isbn: userIsbnValue },
+          }).then((res) => {
+            console.log(res);
+          });
+        }}
+      >
+        요청
+      </button>
     </div>
   );
 }
