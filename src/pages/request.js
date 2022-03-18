77457678a1e0c0654133307fe580eb0ead09ab37
noname2048 <sungwook.csw@gmail.com>
2022-03-18 23:32:03 +0900

    feat: add default value

--- src/pages/request.js
+++ src/pages/request.js
@@ -1,11 +1,42 @@
+import { useState } from "react";
 import IndexHeader from "../components/IndexHeader";
-import NewBookRequest from "../components/NewBookRequest";
+import styles from "./request.module.scss";
 
 export default function Page() {
+  const [userIsbnValue, setUserIsbnValue] = useState("9791165213190");
+
   return (
     <>
       <IndexHeader />
-      <NewBookRequest />
+      <div className={styles.newrequest}>
+        <h1>등록요청하기</h1>
+        <div className={styles.form}>
+          <span>isbn</span>
+          <label>
+            <input
+              type="text"
+              defaultValue={userIsbnValue ? userIsbnValue : ""}
+              onChange={(e) => {
+                setUserIsbnValue(e.target.value);
+              }}
+            />
+            <span>검색</span>
+          </label>
+        </div>
+        <button
+          onClick={() => {
+            //http://localhost:8000/requests
+            fetch("http://localhost:8000/test/echo", {
+              method: "POST",
+              body: JSON.stringify(userIsbnValue),
+            }).then((res) => {
+              console.log(res);
+            });
+          }}
+        >
+          요청
+        </button>
+      </div>
     </>
   );
 }
