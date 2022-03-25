e53dfa3330a0e6b6b97ef835ef56e0a221fbc1ae
noname2048 <sungwook.csw@gmail.com>
2022-03-25 23:33:12 +0900

    fix: fix modal positon, styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,8 +6,8 @@ const cx = classNames.bind(styles);
 export default function Modal(props) {
   const { open, close, header } = props;
   return (
-    <div className={open ? cx("modal open") : cx("modal")}>
-      {props.children}
+    <div className={open ? cx("Modal", "openModal") : cx("Modal")}>
+      <div>{props.children}</div>
       <button onClick={close}>닫기</button>
     </div>
   );
