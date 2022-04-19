0dc0d87d3c0e22e7a1345297a64faa1196571780
noname2048 <sungwook.csw@gmail.com>
2022-04-19 23:27:27 +0900

    feat: split search component

---
+++ src/components/search/TitleSearch.js
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router";
+import { BsSearch } from "react-icons/bs";
+import styles from "./TitleSearch.module.scss";
+
+export default function TitleSearch({ children, props }) {
+  const router = useRouter();
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.h2}>
+        <span className={styles.span}>제목</span>으로 검색하기
+      </h2>
+      <label>
+        <input
+          type="text"
+          onKeyPress={(event) => {
+            if (event.key == "Enter" && titleRef.current.value !== "")
+              router.push(`/titlesearch?title=${event.target.value}`);
+          }}
+          ref={titleRef}
+        />
+        <BsSearch
+          onClick={() => {
+            pushTitleSearch();
+          }}
+        />
+      </label>
+    </div>
+  );
+}
