2ee6679e651297df8fafc739ef09037befd5b64e
noname2048 <sungwook.csw@gmail.com>
2022-04-21 23:31:11 +0900

    feat: make CommonSearch Comp

--- src/components/search/CommonSearch.js
+++ src/components/search/CommonSearch.js
@@ -1,5 +1,39 @@
 import styles from "./CommonSearch.module.scss";
+import { useState } from "react";
+import { Router, useRouter } from "next/router";
 
-export default function CommonSearch() {
-  return <div className={styles.container}></div>;
+export default function CommonSearch({ children, props }) {
+  const [inputValue, setInputValue] = useState("");
+  const resetInputValue = () => {
+    setInputValue("");
+  };
+  const router = useRouter();
+  const pushDetailPage = () => {
+    router.push(`${title}search?${title}=${inputValue}`);
+  };
+  // icon
+  const { svg: Comp } = children;
+
+  return (
+    <div className={styles.container}>
+      <h2>
+        <span>{props.title}</span>으로 검색하기
+      </h2>
+      <label>
+        <input
+          type="text"
+          onKeyDown={(event) => {
+            if (event.key === "Enter" && inputValue !== "") {
+              pushDetailPage();
+            }
+          }}
+        />
+        <Comp
+          onClick={() => {
+            pushDetailPage();
+          }}
+        />
+      </label>
+    </div>
+  );
 }
