a29e3ea35b12598d451216b3742688206342290d
noname2048 <sungwook.csw@gmail.com>
2022-04-22 22:39:11 +0900

    feat: update CommonSearch, add url

--- src/components/search/CommonSearch.js
+++ src/components/search/CommonSearch.js
@@ -3,25 +3,32 @@ import { useState } from "react";
 import { Router, useRouter } from "next/router";
 
 export default function CommonSearch(props) {
-  const { key, verbose, svg: Comp } = props;
+  const { verbose, key, url, svg: Comp, backgroundColor } = props;
   const [inputValue, setInputValue] = useState("");
   const resetInputValue = () => {
     setInputValue("");
   };
   const router = useRouter();
   const pushDetailPage = () => {
-    router.push(`${title}search?${title}=${inputValue}`);
+    router.push(`${url}?${key}=${inputValue}`);
   };
 
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={{ backgroundColor: backgroundColor }}
+    >
       <h2>
-        <span>{props.verbose}</span>으로 검색하기
+        <span>{verbose}</span>으로 검색하기
       </h2>
       <label>
         <input
           type="text"
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
           onKeyDown={(event) => {
+            console.log(event.key);
             if (event.key === "Enter" && inputValue !== "") {
               pushDetailPage();
             }
