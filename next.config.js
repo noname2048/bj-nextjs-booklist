1857ecba187ebd6429c1afccf89e9a859dfe7b8e
noname2048 <sungwook.csw@gmail.com>
2022-04-23 17:12:12 +0900

    feat: add redirects and rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 const path = require("path");
 
 module.exports = {
+  async redirects() {
+    return [{ source: "/", destination: "/search", permanent: false }];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/search",
+      },
+    ];
+  },
   reactStrictMode: true,
   sassOptions: {
     includePath: [path.join(__dirname, "styles")],
