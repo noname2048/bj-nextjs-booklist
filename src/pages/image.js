d10d8b6ea05b79565d9cebd78903aaf812a93e37
noname2048 <sungwook.csw@gmail.com>
2022-04-23 21:28:40 +0900

    research: research about next/images

---
+++ src/pages/image.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+const loader = ({ src, height }) => {
+  return `https://job-book-image.s3.ap-northeast-2.amazonaws.com/9788966261840.jpg`;
+};
+
+export default function Page() {
+  return (
+    <>
+      <div style={{ height: "200px", position: "relative" }}>
+        <Image loader={loader} src="1" alt="" layout="fill" />
+        {/* <img
+          src="https://job-book-image.s3.ap-northeast-2.amazonaws.com/9788966261840.jpg"
+          alt="97889666261840.jpg"
+        /> */}
+      </div>
+    </>
+  );
+}
