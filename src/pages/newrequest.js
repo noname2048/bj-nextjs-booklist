8d5cc77e9899abf128482391b023ac447ba0cbe7
noname2048 <sungwook.csw@gmail.com>
2022-04-26 21:27:55 +0900

    feat: show hint after 3 sec

--- src/pages/newrequest.js
+++ src/pages/newrequest.js
@@ -1,9 +1,33 @@
 import styles from "./newrequest.module.scss";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { GrAdd } from "react-icons/gr";
+
+class BackendNotConnected extends Error {
+  constructor(message) {
+    super.message(message);
+    this.name = "BackendNotConnected";
+  }
+}
 
 export default function Page() {
   const [inputValue, setInputValue] = useState("");
+  const [status, setStatus] = useState("no problem");
+  const [showHint, setShowHint] = useState(false);
+  const timerId = useRef(null);
+  const time = useRef(5);
+  const maintainFocus = useRef(null);
+
+  useEffect(() => {
+    timerId.current = setInterval(() => {
+      time.current = Math.max(time.current - 1, 0);
+      // console.log(time.current);
+      if (time.current <= 0) setShowHint(true);
+      else setShowHint(false);
+    }, 1000);
+    return () => {
+      clearInterval(timerId.current);
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -15,24 +39,52 @@ export default function Page() {
           event.preventDefault();
         }}
       >
+        <div
+          className={`${styles.hint} ${showHint ? "" : styles.hide}`}
+          onClick={() => {
+            setInputValue(9791196700522);
+            maintainFocus.current && maintainFocus.current.focus();
+          }}
+        >
+          {"'9791196700522'로 검색해보기"}
+        </div>
         <label className={styles.label}>
           <input
             className={`${styles.input} ${styles.input}`}
             type="text"
+            value={inputValue}
+            ref={maintainFocus}
             onChange={(event) => {
+              time.current = 5;
               setInputValue(event.target.value);
             }}
             onKeyDown={(event) => {
               if (event.key === "Enter" && inputValue !== "") {
+                const postRequest = async () => {
+                  try {
+                    const res = await fetch("http://localhost:8000/request", {
+                      headers: { "Content-Type": "application/json" },
+                      method: "POST",
+                      body: JSON.stringify({
+                        isbn: inputValue,
+                      }),
+                    });
+                    return res.status;
+                  } catch (err) {
+                    setStatus("backend station!");
+                  }
+                };
+                const status_code = postRequest();
               }
             }}
           />
-          <GrAdd />
+          <GrAdd className={styles.svg} />
         </label>
         <label>
           <span>Update?</span>
           <input type="checkbox" />
         </label>
+        <label>{status}</label>
       </form>
     </div>
   );
