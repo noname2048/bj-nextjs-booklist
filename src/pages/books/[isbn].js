71758792a15eebe7c84dc90564ade082eed2a6d1
noname2048 <sungwook.csw@gmail.com>
2022-06-02 03:41:28 +0900

    chore: fix statisprops to serversideprops

--- src/pages/books/[isbn].js
+++ src/pages/books/[isbn].js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import BookDetail from "../../components/book/BookDetail";
 
+const back = process.env.ORIGIN || "http://localhost:8000";
+
 export default function Books(props) {
   const router = useRouter();
   const param = router.query;
@@ -10,8 +12,6 @@ export default function Books(props) {
   const [responseState, setResponseState] = useState();
   const [book, setBook] = useState();
   const [isError, setIsError] = useState(false);
-
-  const back = process.env.ORIGIN || "http://localhost:8000";
 
   async function fetchBooks(isbn) {
     try {
@@ -27,21 +27,21 @@ export default function Books(props) {
     }
   }
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      fetchBooks(isbn);
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, [responseState, isbn]);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     fetchBooks(isbn);
+  //   }, 3000);
+  //   return () => clearTimeout(timer);
+  // }, [responseState, isbn]);
 
   console.log(props);
   return <>HI{/* <BookDetail book={book} state={responseState} /> */}</>;
 }
 
-function getStaticProps(context) {
-  const { params } = context;
-  const { isbn } = params;
-  return {
-    props: { isbn: isbn },
-  };
+export async function getServerSideProps(context) {
+  const { isbn } = context.query;
+  const res = await fetch(`${back}/book/${isbn}`);
+  const data = await res.json();
+  console.log(data);
+  return { props: { book: data } };
 }
